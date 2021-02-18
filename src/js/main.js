65baba914dd2a9f0e5fda7ff180e893127229650
rickjweaver <42755416+rickjweaver@users.noreply.github.com>
2021-02-17 20:32:19 -0600

    Delete src directory

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-// populate copyright in footer with current year
-const copyrightYear = document.querySelector('.copyright-year');
-const now = new Date();
-copyrightYear.innerHTML = now.getFullYear();
-
-
